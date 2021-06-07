389253e1561bc5bad6a65f7e16a85503fe77cc2d
nitya19 <83870576+nitya19@users.noreply.github.com>
2021-06-07 13:02:18 +0530

    Add files via upload

---
+++ PROGRAMS/CtoF.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    float celsius,fahrenheit;
+    printf("\nEnter temperature in Fahrenheit:");
+    scanf("%f",&fahrenheit);
+    celsius=(fahrenheit - 32)*5/9;
+    printf("\nCelsius = %.3f", celsius);
+}
