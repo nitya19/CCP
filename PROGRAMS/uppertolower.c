3255805115b10b2c4f6b3bec450f0d1ddd0d74e1
nitya19 <83870576+nitya19@users.noreply.github.com>
2021-06-07 11:09:09 +0530

    Add files via upload

---
+++ PROGRAMS/uppertolower.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+
+    char c;
+    printf(" enter a character: ");
+    scanf(" %c", &c);
+    c=c+32;
+    printf(" its lower case is: %c" , c);
+    return 0;
+
+
+
+}
