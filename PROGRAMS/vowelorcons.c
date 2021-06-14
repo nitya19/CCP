e821327d1f3904180a8b7edf60e48f6b3b6ca689
nitya19 <83870576+nitya19@users.noreply.github.com>
2021-06-14 09:49:32 +0530

    Add files via upload

---
+++ PROGRAMS/vowelorcons.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+
+    char c;
+    printf("enter any alphabet: ");
+    scanf("%c", &c);
+    switch(c)
+    {
+     case 'A':
+        printf("vowel");
+        break;
+     case 'a':
+        printf("vowel");
+        break;
+     case 'E':
+        printf("vowel");
+        break;
+     case 'e':
+        printf("vowel");
+        break;
+     case 'I':
+        printf("vowel");
+        break;
+     case 'i':
+        printf("vowel");
+        break;
+     case 'O':
+        printf("vowel");
+        break;
+     case 'o':
+        printf("vowel");
+        break;
+         case 'U':
+        printf("vowel");
+        break;
+     case 'u':
+        printf("vowel");
+        break;
+     default:
+        printf("consonant");
+        break;
+        return 0;
+
+
+    }
+}
